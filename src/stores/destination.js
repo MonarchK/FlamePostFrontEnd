b591f170243d31b01509a379d3c28e8eef2fd96f
Richy <103085436+MonarchK@users.noreply.github.com>
2025-08-28 17:34:05 -0500

    Update destination.js

--- src/stores/destination.js
+++ src/stores/destination.js
@@ -1,1 +1,1 @@
-export default 'https://arbitrary-tove-blajhgxutciyv-4eefb99a.koyeb.app/' //http://localhost:3720  https://flamepostbackend-production.up.railway.app https://flamepostbackend.onrender.com
+export default 'https://arbitrary-tove-blajhgxutciyv-4eefb99a.koyeb.app' //http://localhost:3720  https://flamepostbackend-production.up.railway.app https://flamepostbackend.onrender.com
